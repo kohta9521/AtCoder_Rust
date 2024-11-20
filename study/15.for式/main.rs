c0a3ff38cb70833b2c83802a924eb9761ad2de35
k_95.21 <kohta0421kochi@gmail.com>
2024-11-20 23:41:13 +0900

    add new for lesson

---
+++ study/15.for式/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let primes = [2, 3, 5, 7];
+    println!("{:p}", &primes[0]);
+    println!("{:p}", &primes[1]);
+    println!("{:p}", &primes[2]);
+    println!("{:p}", &primes[3]);
+    println!("{:p}", &primes);
+
+    for p in &primes {
+        println!("{:p}", p);
+    }
+}
