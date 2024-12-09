fb420466dc96e1f3aee6db79d9544a7ce85e58d8
k_95.21 <kohta0421kochi@gmail.com>
2024-12-09 16:25:37 +0900

    finish loop section

--- study/19.さまざまなループ/main.rs
+++ study/19.さまざまなループ/main.rs
@@ -1,10 +1,5 @@
 fn main() {
-    let array = [2, 3, 0, 4, 5];
-    for &i in &array {
-        if i == 0 {
-            break;
-        }
-        print!("{}, ", i);
-    }
-    println!("end");
+ for i in 0..5 {
+    println!("{}", i);
+ }
 }
